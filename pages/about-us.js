efadce4056ddb015e583fc94f02b8af6c8c24430
mosalah <almoaz065@gmail.com>
2022-03-11 14:12:39 +0200

    feat: 🎸 Learn About Routes in Nextjs

---
+++ pages/about-us.js
@@ -0,0 +1,11 @@
+import Head from "next/head";
+export default function About() {
+  return (
+    <div>
+        <Head>
+            <title>About us Page</title>
+        </Head>
+      <h2>About us page</h2>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image"
 
 export default function Home() {
   return (
@@ -10,41 +12,23 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to{" "}
+          <a href="https://nextjs.org">Next.js! i'm mohammed salah</a>
         </h1>
-
+        <div className="profile-image">
+        <Image src="/images/profile.jpg" width={400} height={400}/>
+        </div>
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
 
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <Link href="/about-us" className="card">
+            <div>
+              <h3>About us &rarr;</h3>
+              <p>Find more things about our vision.</p>
+            </div>
+          </Link>
         </div>
       </main>
 
@@ -54,8 +38,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
+          Powered by <img src="/vercel.svg" alt="Vercel" className="logo" />
         </a>
       </footer>
 
@@ -205,5 +188,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
